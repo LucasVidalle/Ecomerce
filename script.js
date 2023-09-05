03fc9df668a0ee1c2c775397c350d028148c5fc5
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-09-05 13:02:27 -0300

    feat:I did a dom to make multiple cards appear

--- script.js
+++ script.js
@@ -1,89 +1,60 @@
-let products = [
+let containerCards;
+const products = [
     {
-        image: "llave-inglesa.png",
-        name: "Lorem ipsum dolor sit amet consectetur",
-        idNumber: "identifer number",
-        oldPrice: "$999",
-        price: "$699",
+        
+        image:"llave-inglesa.png",
+        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+        index:1,
+        oldPrice: "999",
+        price:"699",
     },
-
     {
-        image: "llave-inglesa.png",
-        name: "Lorem ipsum dolor sit amet consectetur",
-        idNumber: "identifer number",
-        oldPrice: "$999",
-        price: "$699",
+        image:"llave-inglesa.png",
+        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+        index:2,
+        oldPrice: "999",
+        price:"699",
     },
-
     {
-        image: "llave-inglesa.png",
-        name: "Lorem ipsum dolor sit amet consectetur",
-        idNumber: "identifer number",
-        oldPrice: "$999",
-        price: "$699",
+        image:"llave-inglesa.png",
+        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+        index:3,
+        oldPrice: "999",
+        price:"699",
+    },
+    {
+        image:"llave-inglesa.png",
+        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+        index:4,
+        oldPrice: "999",
+        price:"699",
     }
-];
+]
 
-array.forEach((producto)=>{
-    const newDiv = document.createElement("div");
-    newDiv.className = "container-cards"
-    const fDiv = document.querySelector(".featured-products");
-    fDiv.appendChild(newDiv);
-    newDiv.innerHTML = `
-    <div class=“info”>
-    <img id=“imgf” src=“${array[0].productImg}” alt=“”>
-    <h3>${array[0].productName}</h3>
-    <p>${array[0].productId}</p>
-  </div>
-  <div class=“bottom”>
-    <button class=“addCart”></button>
-    <div class=“prices”>
-      <p class=“listPrice”></p>
-      <p class=“price”>${array[0].productPrice}</p>
-    </div>
-  </div>
-    `
-
-    `
-    <div class=“info”>
-    <img id=“imgf” src=“${array[0].productImg}” alt=“”>
-    <h3>${array[0].productName}</h3>
-    <p>${array[0].productId}</p>
-  </div>
-  <div class=“bottom”>
-    <button class=“addCart”></button>
-    <div class=“prices”>
-      <p class=“listPrice”></p>
-      <p class=“price”>${array[0].productPrice}</p>
-    </div>
-  </div>
-    `
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener("load", (event) => {
+    products.forEach((producto) => {
+      const tarjetaProducto = document.createElement("div");
+      tarjetaProducto.className = 'card';
+      containerCards = document.querySelector(".container-cards");
+      containerCards.appendChild(tarjetaProducto);
+        tarjetaProducto.innerHTML = `
+        <div class="card-section1">
+            <img class="stillson" src="${producto.image}" alt="">
+            <div class="product-data">
+                <h3 class="name-of-products">${producto.name}</h3>
+                <p class="identifier-number">Identifier number ${producto.index}</p>
+            </div>
+            <p class="previous-price">$${producto.oldPrice}</p>
+        </div>
+        <div class="section-below">
+            <div class="button-cart">
+                <img class="shopping-bag" src="shopping-bag.png" alt="">
+                <img class="plus" src="plus.png" alt="">
+            </div>
+            <div class="card-section2">
+                <p class="actual-price">$${producto.price}</p>
+            </div>
+        </div>
+        `;
+    });
+});
