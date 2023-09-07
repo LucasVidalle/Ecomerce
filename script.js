36d91a127c0cc62af13233d0e2da2657bf8e99ee
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-09-07 13:04:42 -0300

    feat:appy progress

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
 let containerCards;
-const products = [
-    {
+// const PRODUCTS_HARDCODED = [
+//     {
         
-        image:"llave-inglesa.png",
-        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
-        index:1,
-        oldPrice: "999",
-        price:"699",
-    },
-    {
-        image:"llave-inglesa.png",
-        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
-        index:2,
-        oldPrice: "999",
-        price:"699",
-    },
-    {
-        image:"llave-inglesa.png",
-        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
-        index:3,
-        oldPrice: "999",
-        price:"699",
-    },
-    {
-        image:"llave-inglesa.png",
-        name:"Lorem Ipsum Dolor Sit Amet Consectetur",
-        index:4,
-        oldPrice: "999",
-        price:"699",
-    }
-]
+//         image:"llave-inglesa.png",
+//         name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+//         index:1,
+//         oldPrice: "999",
+//         price:"699",
+//     },
+//     {
+//         image:"llave-inglesa.png",
+//         name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+//         index:2,
+//         oldPrice: "999",
+//         price:"699",
+//     },
+//     {
+//         image:"llave-inglesa.png",
+//         name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+//         index:3,
+//         oldPrice: "999",
+//         price:"699",
+//     },
+//     {
+//         image:"llave-inglesa.png",
+//         name:"Lorem Ipsum Dolor Sit Amet Consectetur",
+//         index:4,
+//         oldPrice: "999",
+//         price:"699",
+//     }
+// ]
 
-window.addEventListener("load", (event) => {
-    products.forEach((producto) => {
+async function fetchProducts() {
+    const apiURL = "https://64f659ae2b07270f705e6753.mockapi.io/api/products";
+    const apiResponse = await fetch(apiURL)
+    const jsonData = await apiResponse.json();
+    console.log(apiResponse.status); // 200
+    return jsonData;
+}
+
+window.addEventListener("load", async (event) => {
+
+    const products = await fetchProducts();
+    products.forEach((jsonData) => {
       const tarjetaProducto = document.createElement("div");
       tarjetaProducto.className = 'card';
       containerCards = document.querySelector(".container-cards");
       containerCards.appendChild(tarjetaProducto);
         tarjetaProducto.innerHTML = `
         <div class="card-section1">
-            <img class="stillson" src="${producto.image}" alt="">
+            <img class="stillson" src="${jsonData.image}" alt="">
             <div class="product-data">
-                <h3 class="name-of-products">${producto.name}</h3>
-                <p class="identifier-number">Identifier number ${producto.index}</p>
+                <h3 class="name-of-products">${jsonData.title}</h3>
+                <p class="identifier-number">Identifier number ${jsonData.id}</p>
             </div>
-            <p class="previous-price">$${producto.oldPrice}</p>
+            <p class="previous-price">$${jsonData.previousPrice}</p>
         </div>
         <div class="section-below">
             <div class="button-cart">
@@ -52,7 +62,7 @@ window.addEventListener("load", (event) => {
                 <img class="plus" src="plus.png" alt="">
             </div>
             <div class="card-section2">
-                <p class="actual-price">$${producto.price}</p>
+                <p class="actual-price">$${jsonData.price}</p>
             </div>
         </div>
         `;
